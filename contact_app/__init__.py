bb929be78a4f8815918a8472012ef2077a524815
Remya Rajan <remyaz09@gmail.com>
2020-01-04 00:30:09 +0530

    models and migration files added

--- contact_app/__init__.py
+++ contact_app/__init__.py
@@ -1,5 +1,6 @@
 # third-party imports
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 # local imports
@@ -13,6 +14,9 @@ def create_app_contact_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
+    migrate = Migrate(app, db)
+
+    from contact_app import models
 
     @app.route('/')
     def hello_world():
---
+++ contact_app/models.py
@@ -0,0 +1,18 @@
+from contact_app import db
+
+
+class Contact(db.Model):
+    """
+    Create an Employee table
+    """
+
+    __tablename__ = 'contacts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    phone_number = db.Column(db.Integer(), unique=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
