db142e235ab357e23c500f31268b38ed4990dc35
Remya Rajan <remyaz09@gmail.com>
2020-01-04 14:36:22 +0530

    dynamic db uri config added

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
+def get_db_uri_from_env():
+    import os
+    user = os.environ.get('MYSQL_USER', 'root')
+    password = os.environ.get('MYSQL_PASSWORD', 'password')
+    host = os.environ.get('MYSQL_HOST', 'localhost')
+    port = os.environ.get('MYSQL_PORT', '5432')
+    db_name = os.environ.get('MYSQL_DB_NAME', 'my_simple_db')
+    return 'mysql://{user}:{password}@{host}:{port}/{db_name}'.format(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        db_name=db_name
+    )
+
+
 class Config(object):
     """
     Common configurations
@@ -14,7 +30,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
     SECRET_KEY = 'v#l70zX7ggfEsW!'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:5432/fullcontactapp'
+    SQLALCHEMY_DATABASE_URI = get_db_uri_from_env()
 
 
 class ProductionConfig(Config):
